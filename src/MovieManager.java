91bb5cb3859ddb9879fe0608959b968928c5f221
Isaac Parker <parkerisaac09@gmail.com>
2025-12-06 00:14:57 +0000

    Update MovieManager.java

--- src/MovieManager.java
+++ src/MovieManager.java
@@ -1,10 +1,12 @@
 import java.util.ArrayList;
 public class MovieManager {
     
-    private ArrayList<Movie> movieList = new ArrayList<>();
+    private ArrayList<Movie> movieList;
 
     public MovieManager(){
         this.movieList = new ArrayList<>();
     }
+
+    
 
 }
